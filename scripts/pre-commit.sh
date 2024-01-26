b8f1c33841451b7e153415d29ecc8cedc5449ad6
nwisnewski89 <30911393+nwisnewski89@users.noreply.github.com>
2024-01-26 10:15:52 -0500

    fixup precommit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -6,11 +6,11 @@ DIFF=$(git diff --cached --name-only)
 if [[ "${DIFF}" == *"terraform"* ]]; then
     working_dir=$(pwd)
     for d in terraform/modules/*; do
-        if terraform-docs md "${d}" > "${d}/README.md"; then
-            git add "./${d}/README.md"
-        fi
+        terraform-docs md "${d}" > "${d}/README.md"
         cd ${d}
         terraform fmt
         cd ${working_dir}
     done
 fi
+
+git add .
